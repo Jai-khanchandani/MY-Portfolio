22918a073d7e9b69471a95a9f93a2d88e50b2019
jai <khanchandanijai58@gmail.com>
2022-02-21 13:17:06 +0530

    added new project

---
+++ src/constants/constants.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: 'Covid-19 Tracker',
+    description: "Developed a web application for tracking the Covid-19 cases all over the world. Users can also apply filters on the data and get the desired results",
+      image: '/images/covid.png',
+      tags: ['ReactJS', 'Axios', 'React-Hooks'],
+    source: 'https://github.com/Jai-khanchandani/covid-19-tracker',
+    visit: 'https://numbers-covid19.netlify.app/',
+    id: 0,
+  },
+  {
+    title: 'Recrypto',
+    description:"Created a web application to display in-depth data of all crypto-currencies, where user can check the price , news and other details of a particular crypto-currency and can search forit’s favourite crypto-currrency. React Line-chart is used to display the price of a particular crypto-currency.",
+    image: '/images/recrypto.png',
+    tags: ['ReactJS', 'Redux Toolkit', 'Ant Design', 'ChartJS'],
+    source: 'https://github.com/Jai-khanchandani/recrypto',
+    visit: 'https://recrypto.netlify.app/',
+    id: 1,
+  },
+  {
+    title: 'Lets Order',
+    description:"Created a fully responsive website using Reactjs and CSS media queries for different screen view. user can check out the menu, photo gallery and introductory video.",
+    image: '/images/recrypto.png',
+    tags: ['ReactJS', 'CSS', 'Media-Queries'],
+    source: 'https://github.com/Jai-khanchandani/Restaurant-Application',
+    visit: 'https://gericht-cafe.netlify.app/',
+    id: 1,
+  },
+];
+
+export const experience = [
+  {
+    title: 'Celebal Technologies',
+    description: "Working as Associate Front-end Engineer majorly Doing in JavaScript and ReactJs. And Learning new things daily which is helping me to grow as a good software developer. Also doing hands on NodeJs.",
+      image: '/images/exp1.png',
+      tags: ['ReactJS', 'JavaScript', 'React-Redux'],
+    source: 'https://github.com/Jai-khanchandani/covid-19-tracker',
+    visit: 'https://numbers-covid19.netlify.app/',
+    id: 0,
+  },
+  {
+    title: 'Technoglobe',
+    description:"Done Training in Back-End development with Django and Python. During Training worked on the project ”Volia” which helped me to understand the backend workflow.",
+    image: '/images/exp2.png',
+    tags: ['Python', 'Django', 'SQLite'],
+    source: 'https://github.com/Jai-khanchandani/Slack-Clone',
+    visit: 'https://slack-clone-e2fed.web.app/',
+    id: 1,
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
